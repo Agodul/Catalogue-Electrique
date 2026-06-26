842af4dc6ccc1b55928955acbf9b294edddd7181
Agodul <86412397+Agodul@users.noreply.github.com>
2026-06-26 23:54:14 +0200

    sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "spi-catalogue-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./favicon.ico",
+  "./apple-touch-icon.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
